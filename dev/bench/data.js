85ad5edadbb46f67cb4c144bba2bf836857007f7
github-action-benchmark <github@users.noreply.github.com>
2021-01-17 19:19:54 +0000

    add Benchmark (go) benchmark result for 07807b2007579be58d4da3d96f88662aef1219ad

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1610911194899,
+  "repoUrl": "https://github.com/jimeh/envctl",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "contact@jimeh.me",
+            "name": "Jim Myhrberg",
+            "username": "jimeh"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "07807b2007579be58d4da3d96f88662aef1219ad",
+          "message": "Merge pull request #1 from jimeh/github-actions\n\nci(github): add basic GitHub Actions CI tasks",
+          "timestamp": "2021-01-17T19:19:25Z",
+          "tree_id": "7f2de284af1a6fa38775b97fa3c8de68d3f18c1a",
+          "url": "https://github.com/jimeh/envctl/commit/07807b2007579be58d4da3d96f88662aef1219ad"
+        },
+        "date": 1610911194339,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "Benchmark_parseEnviron_2",
+            "value": 4637,
+            "unit": "ns/op",
+            "extra": "252384 times"
+          }
+        ]
+      }
+    ]
+  }
+}
