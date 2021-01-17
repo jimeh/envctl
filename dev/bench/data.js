d57434bf0ac6d549281d036c20d85fdffc08b800
github-action-benchmark <github@users.noreply.github.com>
2021-01-17 20:50:00 +0000

    add Benchmark (go) benchmark result for dc13f472ea48c8cdf7d6c0ba14aafe441102bf5c

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1610911194899,
+  "lastUpdate": 1610916600091,
   "repoUrl": "https://github.com/jimeh/envctl",
   "entries": {
     "Benchmark": [
@@ -30,6 +30,36 @@ window.BENCHMARK_DATA = {
             "value": 4637,
             "unit": "ns/op",
             "extra": "252384 times"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "contact@jimeh.me",
+            "name": "Jim Myhrberg",
+            "username": "jimeh"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "dc13f472ea48c8cdf7d6c0ba14aafe441102bf5c",
+          "message": "Merge pull request #2 from jimeh/add-readme\n\ndocs(readme): add README and update godoc comments/examples",
+          "timestamp": "2021-01-17T20:49:24Z",
+          "tree_id": "c834416ce6c49fa9e2bd96b0fb4b5bee89a15d2d",
+          "url": "https://github.com/jimeh/envctl/commit/dc13f472ea48c8cdf7d6c0ba14aafe441102bf5c"
+        },
+        "date": 1610916599594,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "Benchmark_parseEnviron_2",
+            "value": 3657,
+            "unit": "ns/op",
+            "extra": "323656 times"
           }
         ]
       }
